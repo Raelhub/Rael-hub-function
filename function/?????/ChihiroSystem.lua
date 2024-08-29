0e0d89b65fac36c70c9ec711ba38b94542f39e94
RaelHub <Israelbiscoitinho@gmail.com>
2024-08-28 21:44:53 -0300

    update ChihiroSystem.lua

--- function/?????/ChihiroSystem.lua
+++ function/?????/ChihiroSystem.lua
@@ -14,6 +14,20 @@ function ChihiroSistema.EncontrarPastaDaChihiro()
     return nil
 end
 
+function ChihiroSistema.EncontrarPataDasCartas()
+    for _, pasta in ipairs(Workspace:GetChildren()) do
+        if pasta:IsA("Folder") then
+            for _, model in ipairs(pasta:GetChildren()) do
+                if model:IsA("Model") and model.Name == "MotherSlender" then
+                    print("Working... " .. pasta.Name)
+                    return pasta
+                end
+            end
+        end
+    end
+    return nil
+end
+
 function ChihiroSistema.EspMonsterMother(folder)
   for _, Pasta in ipairs(folder:GetChildren()) do
     if Pasta:IsA("Folder") then
