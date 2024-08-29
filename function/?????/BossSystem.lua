4aae33f046071c8ebc558eead92e0cb6b7dc312b
RaelHub <Israelbiscoitinho@gmail.com>
2024-08-29 11:35:37 -0300

    update BossSystem.lua

---
+++ function/?????/BossSystem.lua
@@ -0,0 +1,17 @@
+local NagisaSistema = {}
+
+function ChihiroSistema.EncontrarPastaDaChihiro()
+    for _, pasta in ipairs(Workspace:GetChildren()) do
+        if pasta:IsA("Folder") then
+            for _, model in ipairs(pasta:GetChildren()) do
+                if model:IsA("Folder") and model.Name == "Stage2BOSS" then
+                    print("Working... " .. pasta.Name)
+                    return pasta
+                end
+            end
+        end
+    end
+    return nil
+end
+
+return NagisaSistema
