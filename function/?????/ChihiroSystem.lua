c1038f1b7160956ff6cf00b146f05b961ca0b689
RaelHub <Israelbiscoitinho@gmail.com>
2024-08-28 21:00:14 -0300

    update ChihiroSystem.lua

--- function/?????/ChihiroSystem.lua
+++ function/?????/ChihiroSystem.lua
@@ -1,6 +1,6 @@
 local ChihiroSistema = {}
 
-function ChihiroSistema.EncontrarPastaDaTorre()
+function ChihiroSistema.EncontrarPastaDaChihiro()
     for _, pasta in ipairs(Workspace:GetChildren()) do
         if pasta:IsA("Folder") then
             for _, model in ipairs(pasta:GetChildren()) do
@@ -14,6 +14,18 @@ function ChihiroSistema.EncontrarPastaDaTorre()
     return nil
 end
 
-
+function ChihiroSistema.EncontrarPastaDa(folder)
+  for _, Pasta in ipairs(folder:GetChildren()) do
+    if Pasta:IsA("Folder") then
+      for _, Monster in ipairs(Pasta:GetChildren()) do
+        if Monster:IsA("Model") and Monster.Name == "Mother" then
+          local RaelHubEspDestaque = loadstring(game:HttpGet("https://raw.githubusercontent.com/Raelhub/Rael-hub-function/main/function/createesp/No%20Hitbox%20destaque/script.txt"))()
+          RaelHubEspDestaque.EspBixo(Monster, cor)
+          print("Esp working...")
+        end
+      end
+    end
+  end
+end
 
 return ChihiroSistema
