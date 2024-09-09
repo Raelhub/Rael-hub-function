2e19ed5474842623201f4f30bc90b8fbf49afc48
RaelHub <Israelbiscoitinho@gmail.com>
2024-09-09 18:51:15 -0300

    update TranslationSystem main.lua

--- function/??????/TranslationSystem main.lua
+++ function/??????/TranslationSystem main.lua
@@ -1,42 +1,34 @@
 local TranslationModule = {}
 
 function TranslationModule:GetTabs()
-  
     local Texto = {
-      paragrafo = "",
-      btn1 = "",
-      btn2 = ""
-    
+        paragrafo = "",
+        btn1 = "",
+        btn2 = ""
     }
 
     local Players = game:GetService("Players")
     local player = Players.LocalPlayer
     local locale = player.LocaleId:lower()
 
+    -- Verificação de idioma do jogador
     if locale == "pt-br" then
-      
-      Texto.paragrafo = "Você deseja escolher quais versões do script ?"
-      Texto.btn1 = "Normal"
-      Texto.btn2 = "Auto casa yurei"
-      
+        Texto.paragrafo = "Você deseja escolher quais versões do script?"
+        Texto.btn1 = "Normal"
+        Texto.btn2 = "Auto casa yurei"
     elseif locale == "en-us" then
-      
-      Texto.paragrafo = "Do you want to choose which versions of the script ?"
-      Texto.btn1 = "Normal"
-      Texto.btn2 = "Auto house yurei"
-      
+        Texto.paragrafo = "Do you want to choose which versions of the script?"
+        Texto.btn1 = "Normal"
+        Texto.btn2 = "Auto house yurei"
     elseif locale:sub(1, 2) == "es" then
-
-      Texto.paragrafo = "¿Quieres elegir qué versiones del script?"
-      Texto.btn1 = "Normal"
-      Texto.btn2 = "Auto casa yurei"
-
+        Texto.paragrafo = "¿Quieres elegir qué versiones del script?"
+        Texto.btn1 = "Normal"
+        Texto.btn2 = "Auto casa yurei"
     else
-      
-      Texto.paragrafo = "Do you want to choose which versions of the script ?"
-      Texto.btn1 = "Normal"
-      Texto.btn2 = "Auto house yurei"
-        
+        -- Idioma padrão
+        Texto.paragrafo = "Do you want to choose which versions of the script?"
+        Texto.btn1 = "Normal"
+        Texto.btn2 = "Auto house yurei"
     end
 
     return Texto
