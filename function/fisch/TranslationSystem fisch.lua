013240ba70d24852187aff0813a3835a45d84401
Rael Hub <israelbiscoitinho@gmail.com>
2024-12-01 10:34:02 -0300

    update TranslationSystem fisch.lua

--- function/fisch/TranslationSystem fisch.lua
+++ function/fisch/TranslationSystem fisch.lua
@@ -1,6 +1,12 @@
 local RaelHubTradutor = loadstring(game:HttpGet("https://raw.githubusercontent.com/Raelhub/Rael-hub-function/refs/heads/main/RaelHubTranslatorSystem/script.lua"))()
 
-local Text1 = RaelHubTradutor.Tradutor("OBRIGADO POR USAR RAEL HUB")
+getgenv().RaelHubTradutor = true
+
+if getgenv().RaelHubTradutor then
+  local Text1 = RaelHubTradutor.Tradutor("THANK YOU FOR USING RAEL HUB")
+elseif getgenv().RaelHubTradutor == false then
+  local Text1 = "THANK YOU FOR USING RAEL HUB"
+end
 
 local player = game.Players.LocalPlayer
 local playerGui = player:WaitForChild("PlayerGui")
@@ -164,16 +170,17 @@ function TranslationModule:GetTabs()
 
     -- Carregar as traduções do idioma do jogador se já existirem
     local savedConfig = LoadConfig(currentLanguage)
+    
+    if getgenv().RaelHubTradutor then
+      -- Se as traduções já existem para o idioma atual, carregar
+      if savedConfig then
+          wait(1)
+          screenGui:Destroy()
+          return savedConfig.Main, savedConfig.Quest, savedConfig.Eventos, savedConfig.Teleports, savedConfig.Jogador, savedConfig.Mostrar, savedConfig.Creditos
+      end
 
-    -- Se as traduções já existem para o idioma atual, carregar
-    if savedConfig then
-        wait(1)
-        screenGui:Destroy()
-        return savedConfig.Main, savedConfig.Quest, savedConfig.Eventos, savedConfig.Teleports, savedConfig.Jogador, savedConfig.Mostrar, savedConfig.Creditos
-    end
-
-    -- Se as traduções não existem, fazer a tradução e salvar para o idioma atual
-    local Main = {
+      -- Se as traduções não existem, fazer a tradução e salvar para o idioma atual
+      local Main = {
         name = RaelHubTradutor.Tradutor("Main", currentLanguage),
         section1 = RaelHubTradutor.Tradutor("Teleport to fishing area", currentLanguage),
         section2 = RaelHubTradutor.Tradutor("Auto catch the fish", currentLanguage),
@@ -196,32 +203,32 @@ function TranslationModule:GetTabs()
         button2 = RaelHubTradutor.Tradutor("Sell all fish", currentLanguage),
         slide1 = RaelHubTradutor.Tradutor("Delay in selling", currentLanguage),
         slide2 = RaelHubTradutor.Tradutor("Quantity of Crab cage", currentLanguage)
-    }
-    local Quest = {
-      name = RaelHubTradutor.Tradutor("Quest", currentLanguage),
-      section1 = RaelHubTradutor.Tradutor("Automatically buy the trident rod", currentLanguage),
-      button1 = RaelHubTradutor.Tradutor("Auto", currentLanguage) .. " " .. RaelHubTradutor.Tradutor("trident", currentLanguage)
-    }
-    local Eventos = {
-      name = RaelHubTradutor.Tradutor("Events", currentLanguage),
-      section = RaelHubTradutor.Tradutor("Megalodon features", currentLanguage),
-      button = RaelHubTradutor.Tradutor("Teleport to the megalodon", currentLanguage),
-      toggle1 = RaelHubTradutor.Tradutor("Teleport to the megalodon with the boat", currentLanguage),
-      toggle2 = RaelHubTradutor.Tradutor("Esp megalodon", currentLanguage),
-    }
-    local Teleports = {
-      name = RaelHubTradutor.Tradutor("Teleports", currentLanguage),
-      section1 = RaelHubTradutor.Tradutor("Teleport to the islands", currentLanguage),
-      section2 = RaelHubTradutor.Tradutor("Teleport to the npcs", currentLanguage),
-      section3 = RaelHubTradutor.Tradutor("Teleport to the itens", currentLanguage),
-      section4 = RaelHubTradutor.Tradutor("Teleport to the rods", currentLanguage),
-      dropdown1 = RaelHubTradutor.Tradutor("islands", currentLanguage),
-      dropdown2 = RaelHubTradutor.Tradutor("npcs", currentLanguage),
-      dropdown3 = RaelHubTradutor.Tradutor("itens", currentLanguage),
-      dropdown4 = RaelHubTradutor.Tradutor("rods", currentLanguage),
-      button = RaelHubTradutor.Tradutor("Teleport player", currentLanguage),
-    }
-    local Jogador = {
+      }
+      local Quest = {
+        name = RaelHubTradutor.Tradutor("Quest", currentLanguage),
+        section1 = RaelHubTradutor.Tradutor("Automatically buy the trident rod", currentLanguage),
+        button1 = RaelHubTradutor.Tradutor("Auto", currentLanguage) .. " " .. RaelHubTradutor.Tradutor("trident", currentLanguage)
+      }
+      local Eventos = {
+        name = RaelHubTradutor.Tradutor("Events", currentLanguage),
+        section = RaelHubTradutor.Tradutor("Megalodon features", currentLanguage),
+        button = RaelHubTradutor.Tradutor("Teleport to the megalodon", currentLanguage),
+        toggle1 = RaelHubTradutor.Tradutor("Teleport to the megalodon with the boat", currentLanguage),
+        toggle2 = RaelHubTradutor.Tradutor("Esp megalodon", currentLanguage),
+      }
+      local Teleports = {
+        name = RaelHubTradutor.Tradutor("Teleports", currentLanguage),
+        section1 = RaelHubTradutor.Tradutor("Teleport to the islands", currentLanguage),
+        section2 = RaelHubTradutor.Tradutor("Teleport to the npcs", currentLanguage),
+        section3 = RaelHubTradutor.Tradutor("Teleport to the itens", currentLanguage),
+        section4 = RaelHubTradutor.Tradutor("Teleport to the rods", currentLanguage),
+        dropdown1 = RaelHubTradutor.Tradutor("islands", currentLanguage),
+        dropdown2 = RaelHubTradutor.Tradutor("npcs", currentLanguage),
+        dropdown3 = RaelHubTradutor.Tradutor("itens", currentLanguage),
+        dropdown4 = RaelHubTradutor.Tradutor("rods", currentLanguage),
+        button = RaelHubTradutor.Tradutor("Teleport player", currentLanguage),
+      }
+      local Jogador = {
         name = RaelHubTradutor.Tradutor("Player", currentLanguage),
         section1 = RaelHubTradutor.Tradutor("Teleport to players", currentLanguage),
         section2 = RaelHubTradutor.Tradutor("Player Speed", currentLanguage),
@@ -233,9 +240,9 @@ function TranslationModule:GetTabs()
         button = RaelHubTradutor.Tradutor("Teleport to player", currentLanguage),
         toggle1 = RaelHubTradutor.Tradutor("Ativar velocidade", currentLanguage),
         toggle2 = RaelHubTradutor.Tradutor("Walking On Water", currentLanguage)
-    }
+      }
 
-    local Mostrar = {
+      local Mostrar = {
         name = "Esp",
         section1 = RaelHubTradutor.Tradutor("Show objects", currentLanguage),
         section2 = RaelHubTradutor.Tradutor("Show the monsters", currentLanguage),
@@ -243,17 +250,17 @@ function TranslationModule:GetTabs()
         toggle1 = "Esp " .. RaelHubTradutor.Tradutor(" object ", currentLanguage),
         toggle2 = "Esp " .. RaelHubTradutor.Tradutor(" monster ", currentLanguage),
         toggle3 = "Esp " .. RaelHubTradutor.Tradutor(" players ", currentLanguage)
-    }
+      }
 
-    local Creditos = {
+      local Creditos = {
         name = RaelHubTradutor.Tradutor("Credits", currentLanguage),
         section = RaelHubTradutor.Tradutor("Script creator", currentLanguage),
         descricao = RaelHubTradutor.Tradutor("Join my YouTube channel and Discord for new updates", currentLanguage),
         ContentNotify = RaelHubTradutor.Tradutor("The script has been copied to the desktop", currentLanguage)
-    }
+      }
 
-    -- Salvar as traduções para o idioma do jogador
-    local updatedConfig = {
+      -- Salvar as traduções para o idioma do jogador
+      local updatedConfig = {
         Main = Main,
         Quest = Quest,
         Eventos = Eventos,
@@ -261,12 +268,97 @@ function TranslationModule:GetTabs()
         Jogador = Jogador,
         Mostrar = Mostrar,
         Creditos = Creditos
-    }
+      }
 
-    SaveConfig(updatedConfig, currentLanguage)
+      SaveConfig(updatedConfig, currentLanguage)
 
-    screenGui:Destroy()
-    return Main, Quest, Eventos, Teleports, Jogador, Mostrar, Creditos
+      screenGui:Destroy()
+      return Main, Quest, Eventos, Teleports, Jogador, Mostrar, Creditos
+    elseif getgenv().RaelHubTradutor == false then
+      
+      local Main = {
+        name = "Main",
+        section1 = "Teleport to fishing area",
+        section2 = "Auto catch the fish",
+        section3 = "Stay with infinite oxygen",
+        section4 = "Don't be afk",
+        section5 = "Auto do the treasure map",
+        section6 = "Fish sale",
+        section7 = "Auto sell fish",
+        section8 = "Crab cage Features",
+        dropdown1 = "Teleporte Area",
+        dropdown2 = "Fishs",
+        toggle1 = "Auto fisch",
+        toggle2 = "Infinite Oxygen",
+        toggle3 = "Anti Afk",
+        toggle4 = "Auto treasure",
+        toggle5 = "Auto sell",
+        toggle6 = "Auto buy crab cage",
+        toggle7 = "Auto collect crab cage",
+        button1 = "Sell the selected fish",
+        button2 = "Sell all fish",
+        slide1 = "Delay in selling",
+        slide2 = "Quantity of Crab cage"
+      }
+      local Quest = {
+        name = "Quest",
+        section1 = "Automatically buy the trident rod",
+        button1 = "Auto trident"
+      }
+      local Eventos = {
+        name = "Events",
+        section = "Megalodon features",
+        button = "Teleport to the megalodon",
+        toggle1 = "Teleport to the megalodon with the boat",
+        toggle2 = "Esp megalodon"
+      }
+      local Teleports = {
+        name = "Teleports",
+        section1 = "Teleport to the islands",
+        section2 = "Teleport to the npcs",
+        section3 = "Teleport to the itens",
+        section4 = "Teleport to the rods",
+        dropdown1 = "islands",
+        dropdown2 = "npcs",
+        dropdown3 = "itens",
+        dropdown4 = "rods",
+        button = "Teleport player",
+      }
+      local Jogador = {
+        name = "Player",
+        section1 = "Teleport to players",
+        section2 = "Player Speed",
+        section3 = "Pass through objects",
+        section4 = "Light up the map",
+        section5 = "Do you walk on water",
+        dropdowntext = "Players: ",
+        slidetext = "Speed: ",
+        button = "Teleport to player",
+        toggle1 = "Enable speed",
+        toggle2 = "Walking On Water"
+      }
+
+      local Mostrar = {
+        name = "Esp",
+        section1 = "Show objects",
+        section2 = "Show the monsters",
+        section3 = "Show players",
+        toggle1 = "Esp object ",
+        toggle2 = "Esp monster ",
+        toggle3 = "Esp players "
+      }
+
+      local Creditos = {
+        name = "Credits",
+        section = "Script creator",
+        descricao = "Join my YouTube channel and Discord for new updates",
+        ContentNotify = "The script has been copied to the desktop"
+      }
+
+      screenGui:Destroy()
+      return Main, Quest, Eventos, Teleports, Jogador, Mostrar, Creditos
+      
+    end
 end
 
 return TranslationModule
